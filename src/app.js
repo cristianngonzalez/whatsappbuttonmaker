1642930a61167886fe4c34852de0223f05843d03
cristianngonzalez <95558857+cristianngonzalez@users.noreply.github.com>
2022-01-22 17:30:38 -0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Give me follow in instagram ;)',
+      countries: allCountries,
+      countrySelected: '+54',
+      phoneNumber: '',
+      color: '#00CC33',
+    }
+
+
+})
+
---
+++ src/modules/allCountries.js
@@ -0,0 +1,6 @@
+/*Variable for input of vue*/
+let allCountries = [
+    { text: 'Argentina', value: '+54' },
+    { text: 'Chile', value: '+56' },
+    { text: 'United States', value: '+1' }
+]
